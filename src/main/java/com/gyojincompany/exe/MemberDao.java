a85d2e8df718d32747a91c2f1a3173dc8ec6d1dc
gyojincompany <luckyred@gmail.com>
2023-01-25 12:22:46 +0900

    회원 가입 기능 추가

--- src/main/java/com/gyojincompany/exe/MemberDao.java
+++ src/main/java/com/gyojincompany/exe/MemberDao.java
@@ -3,6 +3,7 @@ package com.gyojincompany.exe;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MemberDao {
@@ -49,6 +50,53 @@ public class MemberDao {
 		}
 		
 		return resultFlag;
+	}
+	
+	public int checkId(String id) {
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;		
+		int resultFlag = 0;
+		
+		String sql = "SELECT * FROM members WHERE id = ?";
+		
+		try {
+			Class.forName(driverName);//드라이버 불러오기
+			conn = DriverManager.getConnection(url, username, password);//DB 연동
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			
+			rs = pstmt.executeQuery();//ResultSect 객체인 rs로 받기
+			
+			if(rs.next()) {
+				resultFlag = 1;//가입하려는 아이디가 이미 존재
+			} else {
+				resultFlag = 0;//가입하려는 아이디가 존재하지 않음
+			}
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}				
+			}catch(Exception e2) {
+				e2.printStackTrace();
+			}
+			
+		}
+		
+		return resultFlag;
+				
 	}
 	
 }
