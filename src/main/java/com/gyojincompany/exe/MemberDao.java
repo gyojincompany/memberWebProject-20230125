078e09ea60715c535ab2cead504e1f76643e9c79
gyojincompany <luckyred@gmail.com>
2023-01-25 16:29:19 +0900

    회원 정보 수정 기능 추가

--- src/main/java/com/gyojincompany/exe/MemberDao.java
+++ src/main/java/com/gyojincompany/exe/MemberDao.java
@@ -203,4 +203,44 @@ public class MemberDao {
 		return dto; 
 	}
 	
+	public int modify(String id, String pw, String name, String email) {
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "UPDATE members SET password=?, name=?, email=? WHERE id=?";
+		
+		int resultFlag = 0;
+		
+		try {
+			Class.forName(driverName);//드라이버 불러오기
+			conn = DriverManager.getConnection(url, username, password);//DB 연동
+			
+			pstmt = conn.prepareStatement(sql);			
+			pstmt.setString(1, pw);
+			pstmt.setString(2, name);
+			pstmt.setString(3, email);
+			pstmt.setString(4, id);
+			
+			resultFlag = pstmt.executeUpdate();//성공하면 1로 값이 변경
+		
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();
+				}				
+			}catch(Exception e2) {
+				e2.printStackTrace();
+			}
+			
+		}
+		
+		return resultFlag;//1이면 정보수정 성공
+		
+	}
+	
 }
